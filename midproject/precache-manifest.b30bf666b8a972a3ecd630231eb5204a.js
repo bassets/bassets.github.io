7670834ad6e06d8123b212614890897e1d9fb44f
Bar Hatsor <34835685+barhatsor@users.noreply.github.com>
2020-08-31 18:52:08 +0300

    Added midproj

---
+++ midproject/precache-manifest.b30bf666b8a972a3ecd630231eb5204a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bab5e955c0ae08140a5494135459e3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "701fd208b609bcc2fbd6",
+    "url": "/static/css/main.4ce596e3.chunk.css"
+  },
+  {
+    "revision": "6e20a9b437f22bb9e889",
+    "url": "/static/js/2.5623a243.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5623a243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "701fd208b609bcc2fbd6",
+    "url": "/static/js/main.83964d75.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
